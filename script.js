2d3a035ba9f35666515b9131e378aa6ff17c2362
Desubicator <francopirinoli@gmail.com>
2022-06-09 20:51:01 -0300

    Changes in sintax

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 function computerPlay() {
     let outcomes = ["Rock", "Paper", "Scissors"];
     let computerSelection = outcomes[Math.floor(Math.random() * outcomes.length)];
-    console.log(computerSelection);
+    return computerSelection;
 }
 
 function playRound(playerSelection, computerSelection) {
     if(playerSelection.toUpperCase() === "rock" && computerSelection.toUpperCase() === "rock"){
-        return "Both players have chosen Rock, it is a tie!"
+        return "Both players have chosen Rock, it is a tie!";
     }; else if (playerSelection.toUpperCase() === "paper" && computerSelection.toUpperCase() === "paper") {
-        return "Both players have chosen Paper, it is a tie!"
+        return "Both players have chosen Paper, it is a tie!";
     }; else if (playerSelection.toUpperCase() === "scissors" && computerSelection.toUpperCase() === "scissors") {
-        return "Both players have chosen Scissors, it is a tie!"
+        return "Both players have chosen Scissors, it is a tie!";
     }; else if (playerSelection.toUpperCase() === "scissors" && computerSelection.toUpperCase() === "paper") {
-        return "Player won, Scissors beats Paper!"
+        return "Player won, Scissors beats Paper!";
     }; else if (playerSelection.toUpperCase() === "scissors" && computerSelection.toUpperCase() === "rock") {
-        return "Computer won, Rock beats Scissors!"
+        return "Computer won, Rock beats Scissors!";
     }; else if (playerSelection.toUpperCase() === "paper" && computerSelection.toUpperCase() === "rock") {
-        return "Player won, Paper beats Rock!"
+        return "Player won, Paper beats Rock!";
     }; else if (playerSelection.toUpperCase() === "paper" && computerSelection.toUpperCase() === "scissors") {
-        return "Computer won, Scissors beats Paper!"
+        return "Computer won, Scissors beats Paper!";
     }; else if (playerSelection.toUpperCase() === "rock" && computerSelection.toUpperCase() === "scissors") {
-        return "Player won, Rock beats Scissors!"
+        return "Player won, Rock beats Scissors!";
     }; else if (playerSelection.toUpperCase() === "rock" && computerSelection.toUpperCase() === "paper") {
-        return "Computer won, Paper beats Rock!"
+        return "Computer won, Paper beats Rock!";
     }
 }
 
