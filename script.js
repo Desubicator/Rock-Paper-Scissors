034db584fcd0be89b3282dc3c2d62fb4e8a8e078
Desubicator <francopirinoli@gmail.com>
2022-06-09 20:57:53 -0300

    Bugfixing script

--- script.js
+++ script.js
@@ -5,26 +5,26 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if(playerSelection.toUpperCase() === "rock" && computerSelection.toUpperCase() === "rock"){
+    if(playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "ROCK"){
         return "Both players have chosen Rock, it is a tie!";
-    }; else if (playerSelection.toUpperCase() === "paper" && computerSelection.toUpperCase() === "paper") {
+    } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "PAPER") {
         return "Both players have chosen Paper, it is a tie!";
-    }; else if (playerSelection.toUpperCase() === "scissors" && computerSelection.toUpperCase() === "scissors") {
+    } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "SCISSORS") {
         return "Both players have chosen Scissors, it is a tie!";
-    }; else if (playerSelection.toUpperCase() === "scissors" && computerSelection.toUpperCase() === "paper") {
+    } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "PAPER") {
         return "Player won, Scissors beats Paper!";
-    }; else if (playerSelection.toUpperCase() === "scissors" && computerSelection.toUpperCase() === "rock") {
+    } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "ROCK") {
         return "Computer won, Rock beats Scissors!";
-    }; else if (playerSelection.toUpperCase() === "paper" && computerSelection.toUpperCase() === "rock") {
+    } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK") {
         return "Player won, Paper beats Rock!";
-    }; else if (playerSelection.toUpperCase() === "paper" && computerSelection.toUpperCase() === "scissors") {
+    } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "SCISSORS") {
         return "Computer won, Scissors beats Paper!";
-    }; else if (playerSelection.toUpperCase() === "rock" && computerSelection.toUpperCase() === "scissors") {
+    } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "SCISSORS") {
         return "Player won, Rock beats Scissors!";
-    }; else if (playerSelection.toUpperCase() === "rock" && computerSelection.toUpperCase() === "paper") {
+    } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "PAPER") {
         return "Computer won, Paper beats Rock!";
     }
-}
+};
 
 const playerSelection = "rock";
 const computerSelection = computerPlay();
