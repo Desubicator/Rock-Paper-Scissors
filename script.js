17a9dd6ae0162a8d0ee264048121ccbaaaf56343
Desubicator <francopirinoli@gmail.com>
2022-06-09 22:24:47 -0300

    Added functionality to the game function

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ function computerPlay() {
     return computerSelection;
 }
 
-function playRound(playerSelection, computerSelection) {
-    let computerScore = 0;
-    let playerScore = 0;
+function game() {
+
+  let computerScore = 0;
+  let playerScore = 0;
+  let rounds = parseInt(prompt("Choose amount of rounds"));
+
+  function playRound(playerSelection, computerSelection) {
 
     if(playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "ROCK"){
         return "Both players have chosen Rock, it is a tie!";
@@ -15,40 +19,42 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "SCISSORS") {
         return "Both players have chosen Scissors, it is a tie!";
     } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "PAPER") {
-        playerScore +=1;
+        playerScore++;
         return "Player won, Scissors beats Paper!";
     } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "ROCK") {
-        computerScore +=1;
+        computerScore++;
         return "Computer won, Rock beats Scissors!";
     } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK") {
-        playerScore +=1;
+        playerScore++;
         return "Player won, Paper beats Rock!";
     } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "SCISSORS") {
-        computerScore +=1;
+        computerScore++;
         return "Computer won, Scissors beats Paper!";
     } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "SCISSORS") {
-        playerScore +=1;
+        playerScore++;
         return "Player won, Rock beats Scissors!";
     } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "PAPER") {
-        computerScore +=1;
+        computerScore++;
         return "Computer won, Paper beats Rock!";
     }
 };
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound(playerSelection, computerSelection);
-        console.log(playRound(playerSelection, computerSelection))
-     }
-     if (playerScore === computerScore) {
-        return `Player Score: ${playerScore}, Computer Score: ${computerScore}, Tie Game!`
-     } else if (playerScore < computerScore) {
-        return `Player Score: ${playerScore}, Computer Score: ${computerScore}, Computer Wins!`
-     } else {
-        return `Player Score: ${playerScore}, Computer Score: ${computerScore}, Player Wins!`
-     }
-}
+    for (let i = 0; i < rounds; i++) {
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+        const playerSelection = prompt("Choose between Rock, Paper and Scissors");
+        const computerSelection = computerPlay();
+
+        playRound(playerSelection, computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+        
+     }
+
+     if (playerScore === computerScore) {
+        return `Player Score: ${playerScore/2}, Computer Score: ${computerScore/2}, Tie Game!`
+     } else if (playerScore < computerScore) {
+        return `Player Score: ${playerScore/2}, Computer Score: ${computerScore/2}, Computer Wins!`
+     } else {
+        return `Player Score: ${playerScore/2}, Computer Score: ${computerScore/2}, Player Wins!`
+     }
+    
+};
