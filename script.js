8e01e1378b37169dc374cc1a5e69a061adc11de8
Desubicator <francopirinoli@gmail.com>
2022-06-18 16:04:27 -0300

    added css file

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function computerPlay() {
     let choices = ['rock', 'paper', 'scissors']
     return choices[Math.floor(Math.random() * choices.length)]
 }
+
 function playRound(playerSelection) {
     let computerSelection = computerPlay()
     let result = ""
